6fdbe788d395d97d4cf4513c664320ef6f04bf27
CF <christopher.forgeron@microvision.fr>
2026-05-06 14:32:42 +0200

    [EVO] Envoi de Msvfw32 dans NativeMethods

--- Project/AVI/DrawDibDC.cs
+++ Project/AVI/DrawDibDC.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
+﻿using Microvision.NativeMethods;
 using Microvision.Types;
 
 namespace Microvision.Avi
@@ -17,28 +16,6 @@ namespace Microvision.Avi
         // 13.04.22 : (libs 3.0)
         // ***************************************************************************************************
 
-        [DllImport("Msvfw32.dll")] private static extern bool DrawDibBegin(IntPtr hdd, IntPtr hdc, int dxDest, int dyDest, IntPtr lpbi, int dxSrc, int dySrc, int wFlags);
-        [DllImport("Msvfw32.dll")] private static extern bool DrawDibClose(IntPtr hdd);
-        [DllImport("Msvfw32.dll")] private static extern bool DrawDibDraw(IntPtr hdd, IntPtr hdc, int xDst, int yDst, int dxDst, int dyDst, IntPtr lpbi, IntPtr lpBits, int xSrc, int ySrc, int dxSrc, int dySrc, int wFlags);
-        [DllImport("Msvfw32.dll")] private static extern bool DrawDibEnd(IntPtr hdd);
-        [DllImport("Msvfw32.dll")] private static extern IntPtr DrawDibGetBuffer(IntPtr hdd, IntPtr lpbi, int dwSize, int dwFlags);
-        [DllImport("Msvfw32.dll")] private static extern IntPtr DrawDibOpen();
-        [DllImport("Msvfw32.dll")] private static extern uint DrawDibRealize(IntPtr hdd, IntPtr hdc, int fBackground);
-
-
-        private const short DDF_UPDATE = 2; // /* re-draw the last DIB */
-        private const short DDF_SAME_HDC = 4; // /* HDC same As last call (all setup) */
-        private const short DDF_SAME_DRAW = 8; // /* draw params are the same */
-        private const short DDF_DONTDRAW = 16; // /* dont draw frame, just decompress */
-        private const short DDF_ANIMATE = 32; // /* allow palette animation */
-        private const short DDF_BUFFER = 64; // /* always buffer image */
-        private const short DDF_JUSTDRAWIT = 128; // /* just draw it with GDI */
-        private const short DDF_FULLSCREEN = 256; // /* use DisplayDib */
-        private const short DDF_BACKGROUNDPAL = 512; // /* Realize palette in background */
-        private const short DDF_NOTKEYFRAME = 1024; // /* this is a partial frame update, hint */
-        private const short DDF_HURRYUP = 2048; // /* hurry up please! */
-        private const short DDF_HALFTONE = 4096; // /* always halftone */
-
 
         private IntPtr _handle;
         private IntPtr _hDC;         // -- entre BeginDraw et EndDraw.
@@ -50,7 +27,7 @@ namespace Microvision.Avi
 
         public DrawDibDC() : base()
         {
-            _handle = DrawDibOpen();
+            _handle = Msvfw32.DrawDibOpen();
         }
 
 
@@ -68,7 +45,7 @@ namespace Microvision.Avi
             _hDC = gf.GetHdc();
 
             IntPtr hbmi = MarshShop.LockStruct(bmi);
-            bool ok = DrawDibBegin(_handle, _hDC, -1, -1, hbmi, -1, -1, 0);
+            bool ok = Msvfw32.DrawDibBegin(_handle, _hDC, -1, -1, hbmi, -1, -1, 0);
             bmi = MarshShop.UnlockStruct<NativeMethods.Gdi32.BITMAPINFO>(hbmi);
 
             return ok;
@@ -79,7 +56,7 @@ namespace Microvision.Avi
             _hDC = gf.GetHdc();
 
             IntPtr hbmi = MarshShop.LockStruct(bmi);
-            bool ok = DrawDibBegin(_handle, _hDC, siz.Width, siz.Height, hbmi, -1, -1, 0);
+            bool ok = Msvfw32.DrawDibBegin(_handle, _hDC, siz.Width, siz.Height, hbmi, -1, -1, 0);
             bmi = MarshShop.UnlockStruct<NativeMethods.Gdi32.BITMAPINFO>(hbmi);
 
             return ok;
@@ -94,7 +71,7 @@ namespace Microvision.Avi
                 // DDF_SAME_DRAW incompatible avec zoom
                 IntPtr hbmi = MarshShop.LockStruct(bmi);
                 LockTable<byte> hbf = new LockTable<byte>(bts.Array, bts.Length);
-                ok = DrawDibDraw(_handle, _hDC, p.X, p.Y, -1, -1, hbmi, hbf.Address(0), 0, 0, -1, -1, DDF_SAME_HDC);
+                ok = Msvfw32.DrawDibDraw(_handle, _hDC, p.X, p.Y, -1, -1, hbmi, hbf.Address(0), 0, 0, -1, -1, Msvfw32.DDF_SAME_HDC);
                 hbf.Free();
                 bmi = MarshShop.UnlockStruct<NativeMethods.Gdi32.BITMAPINFO>(hbmi);
             }
@@ -111,7 +88,7 @@ namespace Microvision.Avi
                 // DDF_SAME_DRAW incompatible avec zoom
                 IntPtr hbmi = MarshShop.LockStruct(bmi);
                 LockTable<byte> hbf = new LockTable<byte>(bts.Array, bts.Length);
-                ok = DrawDibDraw(_handle, _hDC, rct.X, rct.Y, rct.Width, rct.Height, hbmi, hbf.Address(0), 0, 0, -1, -1, DDF_SAME_HDC);
+                ok = Msvfw32.DrawDibDraw(_handle, _hDC, rct.X, rct.Y, rct.Width, rct.Height, hbmi, hbf.Address(0), 0, 0, -1, -1, Msvfw32.DDF_SAME_HDC);
                 hbf.Free();
                 bmi = MarshShop.UnlockStruct<NativeMethods.Gdi32.BITMAPINFO>(hbmi);
             }
@@ -122,7 +99,7 @@ namespace Microvision.Avi
         public bool EndDraw(Graphics gf)
         {
             gf.ReleaseHdc(_hDC);        // -- nouveau 2.0
-            bool ok = DrawDibEnd(_handle);
+            bool ok = Msvfw32.DrawDibEnd(_handle);
 
             return ok;
         }
@@ -136,7 +113,7 @@ namespace Microvision.Avi
         {
             if (_handle != IntPtr.Zero)
             {
-                DrawDibClose(_handle);
+                Msvfw32.DrawDibClose(_handle);
                 _handle = IntPtr.Zero;
             }
 
