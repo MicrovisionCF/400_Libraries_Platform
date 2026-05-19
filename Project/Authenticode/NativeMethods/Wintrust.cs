862052cd1859bd8cabf974c917795eee29987d0f
CF <christopher.forgeron@microvision.fr>
2026-05-19 18:04:21 +0200

    [EVO] Ajout Authenticode

---
+++ Project/Authenticode/NativeMethods/Wintrust.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microvision.NativeMethods
+{
+    internal class Wintrust
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct WINTRUST_FILE_INFO
+        {
+            public int cbStruct;
+            public string pcwszFilePath;
+            public IntPtr hFile;
+            public IntPtr pgKnownSubject;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WINTRUST_DATA
+        {
+            public int cbStruct;
+            public IntPtr pPolicyCallbackData;
+            public IntPtr pSIPClientData;
+            public int dwUIChoice;
+            public int fdwRevocationChecks;
+            public int dwUnionChoice;
+            public IntPtr pFile;
+            public int dwStateAction;
+            public IntPtr hWVTStateData;
+            public IntPtr pwszURLReference;
+            public int dwProvFlags;
+            public int dwUIContext;
+            public IntPtr pSignatureSettings;
+        }
+
+
+        [DllImport(nameof(Wintrust))]
+        public static extern int WinVerifyTrust(IntPtr hwnd, [MarshalAs(UnmanagedType.LPStruct)] Guid pgActionID, ref WINTRUST_DATA pWVTData);
+
+
+        public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+        public static readonly Guid WINTRUST_ACTION_GENERIC_VERIFY_V2 = new Guid("{00AAC56B-CD44-11d0-8CC2-00C04FC295EE}");
+
+
+        public const int WTD_UI_NONE = 2;
+        public const int WTD_REVOKE_NONE = 0;
+        public const int WTD_CHOICE_FILE = 1;
+
+    }
+}
---
+++ Project/Authenticode/SignShop.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.InteropServices;
+
+using Microvision.NativeMethods;
+
+namespace Microvision.Authenticode
+{
+    public class SignShop
+    {
+        // ***************************************************************************************************
+        // 11.10.23 : Création
+        // 13.05.26 : (libs 4.0) Envoi des bases vers Platform
+        // ***************************************************************************************************
+
+
+        // ----------------------------------------
+        // Classe
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Propriétés
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Statiques
+        // ----------------------------------------
+
+        public static bool IsSigned(string filePath)
+        {
+            ArgumentNullException.ThrowIfNull(filePath);
+
+            Wintrust.WINTRUST_FILE_INFO file = new Wintrust.WINTRUST_FILE_INFO();
+            file.cbStruct = Marshal.SizeOf(typeof(Wintrust.WINTRUST_FILE_INFO));
+            file.pcwszFilePath = filePath;
+
+            Wintrust.WINTRUST_DATA data = new Wintrust.WINTRUST_DATA();
+            data.cbStruct = Marshal.SizeOf(typeof(Wintrust.WINTRUST_DATA));
+            data.dwUIChoice = Wintrust.WTD_UI_NONE;
+            data.dwUnionChoice = Wintrust.WTD_CHOICE_FILE;
+            data.fdwRevocationChecks = Wintrust.WTD_REVOKE_NONE;
+            data.pFile = Marshal.AllocHGlobal(file.cbStruct);
+            Marshal.StructureToPtr(file, data.pFile, false);
+
+            int hr;
+
+            try
+            {
+                hr = Wintrust.WinVerifyTrust(Wintrust.INVALID_HANDLE_VALUE, Wintrust.WINTRUST_ACTION_GENERIC_VERIFY_V2, ref data);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(data.pFile);
+            }
+
+            return hr == 0;
+        }
+
+
+        // ----------------------------------------
+        // Méthodes
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Semi-privées
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Privées
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Evènements
+        // ----------------------------------------
+
+
+        // ----------------------------------------
+        // Implémentations
+        // ----------------------------------------
+
+    }
+}
